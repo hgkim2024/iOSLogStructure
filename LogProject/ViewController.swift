0c5b92d39343ebfa2afb6d15b807718a7955f031
김현구 <auskim20@gmail.com>
2024-03-15 15:55:11 +0900

    Initial Commit

---
+++ LogProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogProject
+//
+//  Created by 김현구 on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
